473197bc93f849144f1a07b97ca0f88e8d3548d2
kencinder <3614897+kencinder@users.noreply.github.com>
2026-07-22 22:44:51 -0400

    Delete igirst/scripts/command-backup.sh

--- igirst/scripts/command-backup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-clear
-
-./igir $command "$backup" --dat "$dat" --input "$input" --output "$output" -R $regions -L $lang -X "$exclude" $options --report-output "$report"
-
-printf "\n"
-read -n 1 -s -r -p "Press any key to continue..."
