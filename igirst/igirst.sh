360aabe2e40c4855ad40c4da074335dc29999080
kencinder <3614897+kencinder@users.noreply.github.com>
2026-08-12 18:54:44 -0400

    Dynamic Menu improvements
    Changed:
    Altered menus for better dynamic support. Should be as easy as adding Platform and System names, and system vars to add a new system and/or platform.
    The menu lists are now generated based on platforms and systems found defined in menu_vars.inc
    
    Changed the system vars to start with the platform name. Ran into issues trying to work with vars that start with numbers, which require an underscore and mixed with ones that don't, when creating dynamic menus.
    
    Reworked several functions to use and/or clear local vars, things worked fine until a subsequent run of the same command would stack vars or wipe them out, or replace them with incorrect ones.
    
    With dynamic menus I no longer needed text lists, so I moved the messages into menu_vars.inc and deleted messages.inc
    
    7z_functions changed to work better with dynamic menus and to move files to the correct location.
    One of the cases had extracted roms left behind in backup and not packed and moved back to the input directory for IGIR to check again.
    
    I tested considerably, but there may still be some edge cases I missed.

--- igirst/igirst.sh
+++ igirst/igirst.sh
@@ -5,7 +5,6 @@ set -a
 source "igirst.cfg"
 source "includes/7z_functions.inc"
 source "includes/menus.inc"
-source "includes/messages.inc"
 source "includes/menu_functions.inc"
 source "includes/menu_vars.inc"
 set +a
@@ -16,7 +15,7 @@ FILE="$igir_binary"
 if [ -f "$FILE" ]; then
 	main_menu
 else
-    printf '%s' "$no_igir"
-    read -n 1 -s -r -p "Press any key to exit..." && clear
+    clear && printf '\n%b\n\n' "$no_igir"
+    read -rsn 1 -p "Press any key to exit..." && clear
     exit
 fi
